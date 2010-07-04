5a4d962f328f799cd7ecb6bebca52d6866a2fa3d
Christopher Cunningham <ccunni@cornuthaum.cs.utexas.edu>
2010-07-04 16:51:41 -0500

    done.

--- Date.h
+++ Date.h
@@ -16,6 +16,13 @@
 #include <stdexcept> // invalid_argument
 #include <utility>   // !=, <=, >, >=
 
+
+
+const int monthDaysSum[] = {0,0,31,59,90,120,151,181,212,243,273,304,334};
+const int monthDays[] =      {0,31,28,31,30, 31, 30, 31, 31, 30, 31, 30, 31};
+const int leapMonthDays[] =  {0,31,29, 31,30, 31, 30, 31, 31, 30, 31, 30, 31};
+const std::string monthNames[] = {"Pad","Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"};
+
 /*
 namespace std     {
 namespace rel_ops {
@@ -52,24 +59,40 @@ using namespace std::rel_ops;
 
 template <typename T>
 class Date {
+        
+        // helper structure 
+        struct DateParts
+        {
+            int month;
+            int day;
+            T year;
+        };
+        
     public:
         // ----------
         // operator +
         // ----------
 
         /**
-         * <your documentation>
+         * @return a new Date computed by adding a given number of days to the original date.
          * @throws invalid_argument if the resulting date precedes 1 Jan 1600
          */
         friend Date operator + (Date lhs, const T& rhs) {
             return lhs += rhs;}
+
+        /**
+         * @return a new Date computed by adding a given number of days to the original date.
+         * @throws invalid_argument if the resulting date precedes 1 Jan 1600
+         */
+        friend Date operator + (const T& lhs, Date rhs) {
+            return rhs += lhs;}
 
         // ----------
         // operator -
         // ----------
 
         /**
-         * <your documentation>
+         * @return a new Date computed by subtracting the given number of days from the given Date.
          * @throws invalid_argument if the resulting date precedes 1 Jan 1600
          */
         friend Date operator - (Date lhs, const T& rhs) {
@@ -84,30 +107,29 @@ class Date {
          * @param  rhs a date
          * @return the ostream
          * output a string representation of the date (e.g. "3 Feb 2008")
-         * you can loop through an array of month names
          */
         friend std::ostream& operator << (std::ostream& lhs, const Date& rhs) {
-            // <your code>
-            return lhs << "1 Jan 2008";}
+            DateParts d = rhs.to_date();
+            return lhs<<d.day << " " << monthNames[d.month] <<" "<< d.year ;
+        }
 
     private:
         // ----
         // data
         // ----
 
-        // <your data>
-
+        T days;
+        
     private:
         // -----
         // valid
         // -----
 
         /**
-         * <your documentation>
+         * @return true if current date is valid.
          */
         bool valid () const {
-            // <your code>
-            return true;}
+            return days >= 0;}
 
         // -----------
         // constructor
@@ -116,11 +138,10 @@ class Date {
         /**
          * @param days >= 0
          * Date(0) -> 1 Jan 1600
-         * you can loop through an array of month days
          */
         Date (const T& days) {
             assert(days >= 0);
-            // <your code>
+            this->days = days;
             if (!valid())
                 throw std::invalid_argument("Date::Date()");}
 
@@ -133,10 +154,34 @@ class Date {
          * 1 Jan 1600 -> 0
          */
         T to_days () const {
-            T days = 0;
-            // <your code>
-            assert(days >= 0);
+            assert(valid());
             return days;}
+            
+        /**
+         * @return DateParts representation of current date
+         * helps with printing and answering questions about leap year status of current date.
+         */
+        DateParts to_date () const {
+            DateParts result;
+            result.day = 1;
+            result.year = 1600;
+            result.month = 1;
+            T tempDays = days+1;
+            
+            while((tempDays>=366 && (result.year%4 == 0 && (result.year%400== 0 || result.year%100!=0))) || tempDays>=365)
+            {
+                tempDays-= ((result.year%4 == 0 && (result.year%400== 0 || result.year%100!=0))) ? 366 : 365;
+                result.year++;
+            }
+            bool isLeapYear =((result.year%4 == 0 && (result.year%400== 0 || result.year%100!=0)));
+            while((tempDays > monthDays[result.month] && !isLeapYear) || (tempDays > leapMonthDays[result.month] && isLeapYear))
+            {
+                tempDays-= isLeapYear ? leapMonthDays[result.month] : monthDays[result.month];
+                ++result.month;
+            }
+            result.day=tempDays;
+            return result;
+        }
 
     public:
         // -----------
@@ -145,14 +190,60 @@ class Date {
 
         /**
          * @param  day   >= 1 && <= 31
-         * @param  month >= 1 && <= 12
+         * @param  month >= 1 && <= 1(year%100 !=0 || year%400 == 0)) && month==2 && day <=29)2
          * @param  year  >= 1600
          * @throws invalid_argument if the resulting date is invalid
          */
-        Date (const T& day, const T& month, const T& year) {
-            // <your code>
-            if (!valid())
-                throw std::invalid_argument("Date::Date()");}
+        Date (const T& day, const T& month, const T& year) : days(0){
+        	using namespace std;
+
+        	bool isLeapYear = (year% 4 == 0 && (year%100 !=0 || year%400 == 0));
+        	if(year >= 1600 && month > 0 && month < 13 && day >=1)
+        	{
+
+				if(day <= monthDays[month] || (isLeapYear && month==2 && day<=29)) 
+				{
+					days+=day-1;
+					days+=monthDaysSum[month];
+					if(isLeapYear && month>2)   //passed leap day in the current year
+					   ++days;
+				}
+				else
+				{
+					throw std::invalid_argument("Date::Date()");
+				}
+				
+				int x400 = (year - 1600) / 400;
+				days += 97*x400;
+
+				int x100 = (year - 1600-x400*400) / 100;
+				days +=  x100>0 ? 1 + 24*x100 : 0;
+
+				int x4 = (year - 1600-x400*400 - x100*100) / 4;
+				int modx4 = (year - 1600-x400*400 - x100*100) % 4;
+
+				if((1600 + x400*400 + x100*100) % 100 == 0 && 
+				    (1600 + x400*400 + x100*100) % 400 != 0  && x4>0) 
+				    x4--; //decrement if first 4 grouping starts on non leap year.
+
+				days += x4;
+                
+                int wasYear = year - modx4; 
+                bool wasLeapYear = (modx4 >= 1) && (wasYear%4 == 0 && (wasYear%100 != 0 || wasYear%400 == 0));
+
+				if(wasLeapYear)
+				{
+					++days; 
+				}
+                
+				days+=(year-1600)*365;
+        	}
+        	else
+        	{
+        		throw std::invalid_argument("Date::Date()");
+        	}
+            assert(valid());
+        }
 
         // Default copy, destructor, and copy assignment.
         // Date (const Date&);
@@ -164,35 +255,37 @@ class Date {
         // -----------
 
         /**
-         * <your documentation>
+         * @param rhs Date object to comapre with
+         * @return true if the two dates are the same
          */
         bool operator == (const Date& rhs) const {
-            // <your code>
-            return false;}
+            return this->days == rhs.days;}
 
         // ----------
         // operator <
         // ----------
 
         /**
-         * <your documentation>
+         * @param rhs Date object to compare with
+         * @return true if "this" date is futher in the past then rhs
          */
         bool operator < (const Date& rhs) const {
-            // <your code>
-            return false;}
+            return this->days < rhs.days;}
 
         // -----------
         // operator +=
         // -----------
 
         /**
-         * <your documentation>
+         * Add the given days to "this" date.
          * @param  days the number of days to add (may be negative!)
          * @return the date resulting from adding days
          * @throws invalid_argument if the resulting date precedes 1 Jan 1600
          */
         Date& operator += (const T& days) {
-            // <your code>
+            if(this->days + days < 0)
+        		throw std::invalid_argument("Date::+=");
+        	this->days += days;
             return *this;}
 
         // -----------
@@ -200,13 +293,16 @@ class Date {
         // -----------
 
         /**
-         * <your documentation>
+         * Subtract the given days from "this" date.
          * @param  days the number of days to subtract (may be negative!)
-         * @return the date resulting from subtracting days
+         * @returif (!valid())
+                throw std::n the date resulting from subtracting days
          * @throws invalid_argument if the resulting date precedes 1 Jan 1600
          */
         Date& operator -= (const T& days) {
-            // <your code>
+            if(this->days - days < 0)
+        		throw std::invalid_argument("Date::-=");
+        	this->days -= days;
             return *this;}
 
         // ----------
@@ -214,22 +310,32 @@ class Date {
         // ----------
 
         /**
-         * <your documentation>
          * @return the number of days between the dates (lhs - rhs)
          */
         T operator - (const Date& rhs) const {
-            // <your code>
-            return 0;}
+            return this->days - rhs.days;}
+
+        // --------
+        // get_days
+        // --------
+        /**
+         * @ return the number of days from 1,1,1600 to current date
+         */
+        T get_days() const
+        {
+            return to_days();
+        }
 
         // ---------
         // leap_year
         // ---------
 
         /**
-         * <your documentation>
+         * @return true if current date is leap year, false otherwise
          */
         bool leap_year () const {
-            // <your code>
-            return false;}};
+            int year = to_date().year;
+            return (year% 4 == 0 && (year%100 !=0 || year%400 == 0));
+            }};
 
 #endif // Date_h
--- TestDate.c++
+++ TestDate.c++
@@ -36,12 +36,86 @@ struct TestDate : CppUnit::TestFixture {
     // ----------------
 
     void test_constructor () {
-        try {
+    	try {
             const Date<int> x(0, 1, 1600);
             CPPUNIT_ASSERT(false);}
         catch (std::invalid_argument& e) {
-            CPPUNIT_ASSERT(std::strcmp(e.what(), "Date()") == 0);}}
+            CPPUNIT_ASSERT(std::strcmp(e.what(), "Date::Date()") == 0);}
+        
+        try {
+            const Date<int> x(29, 2, 2017);
+            CPPUNIT_ASSERT(false);}
+        catch (std::invalid_argument& e) {
+           CPPUNIT_ASSERT(std::strcmp(e.what(), "Date::Date()") == 0);}
 
+        try {
+            const Date<int> x(29, 2, 2000);
+        }
+        catch (std::invalid_argument& e) {
+            CPPUNIT_ASSERT(false);}
+	}
+	
+	// -------------
+	// test_to_days
+	// -------------
+	void test_to_days () {
+	    try
+	    {
+	        const Date<int> b(1, 1, 1600); //0
+	        CPPUNIT_ASSERT(b.get_days() == 0);
+	                
+            const Date<int> y(2, 1, 1601); //367
+   	        CPPUNIT_ASSERT(y.get_days() == 367);
+	                
+            const Date<int> z(2, 1, 3601); //730852
+	        CPPUNIT_ASSERT(z.get_days() == 730852);
+	                            
+            const Date<int> a(1, 3, 2000); //146157
+	        CPPUNIT_ASSERT(a.get_days() == 146157);
+	                
+            const Date<int> c(1, 1, 2400); //292194
+	        CPPUNIT_ASSERT(c.get_days() == 292194);
+	                
+            const Date<int> d(1, 1, 2404); //293655
+	        CPPUNIT_ASSERT(d.get_days() == 293655);
+	                
+            const Date<int> e(1, 3, 2600); //365302
+	        CPPUNIT_ASSERT(e.get_days() == 365302);
+	                
+            const Date<int> f(1, 3, 2601); //365667
+	        CPPUNIT_ASSERT(f.get_days() == 365667);
+	                
+            const Date<int> h(1, 3, 3612); //734928
+	        CPPUNIT_ASSERT(h.get_days() == 734928);
+	                
+            const Date<int> g(1, 3, 3613); //735293
+	        CPPUNIT_ASSERT(g.get_days() == 735293);
+	                
+            const Date<int> ee(1,1,3650);  //748748
+	        CPPUNIT_ASSERT(ee.get_days() == 748748);
+	                
+            const Date<int> aa(1,1,3601);  //730851
+	        CPPUNIT_ASSERT(aa.get_days() == 730851);
+	                
+            const Date<int> bb(1,1,3604);  //731946
+	        CPPUNIT_ASSERT(bb.get_days() == 731946);
+	                
+            const Date<int> cc(1,1,3700);  //767010
+	        CPPUNIT_ASSERT(cc.get_days() == 767010);
+	                
+            const Date<int> dd(1,1,3600);  //730485
+	        CPPUNIT_ASSERT(dd.get_days() == 730485);
+	                
+            const Date<int> ff(1,1,3705);  //768836
+	        CPPUNIT_ASSERT(ff.get_days() == 768836);
+
+            const Date<int> xx(7,12,1848);  //90921
+	        CPPUNIT_ASSERT(xx.get_days() == 90921);
+        }        
+        catch (std::invalid_argument& e) {
+            CPPUNIT_ASSERT(false);}
+        
+    }
     // -------------
     // test_equal_to
     // -------------
@@ -78,9 +152,16 @@ struct TestDate : CppUnit::TestFixture {
         try {
             const Date<int> x(1, 1, 2000);
             const Date<int> y(1, 1, 2001);
-            CPPUNIT_ASSERT(x + 365 == y);}
+            CPPUNIT_ASSERT(x + 366 == y);}
         catch (std::invalid_argument& e) {
-            CPPUNIT_ASSERT(false);}}
+            CPPUNIT_ASSERT(false);}
+        try {
+            const Date<int> x(1, 1, 2000);
+            const Date<int> y(1, 1, 2001);
+            CPPUNIT_ASSERT(366 + x == y);}
+        catch (std::invalid_argument& e) {
+            CPPUNIT_ASSERT(false);}            
+            }
 
     // ----------
     // test_minus
@@ -101,9 +182,60 @@ struct TestDate : CppUnit::TestFixture {
     void test_output () {
         try {
             const Date<int> x(16, 6, 2008);
-            std::ostringstream out;
-            out << x;
-            CPPUNIT_ASSERT(out.str() == "16 Jun 2008");}
+            std::ostringstream out1;
+            out1 << x;
+            CPPUNIT_ASSERT(out1.str() == "16 Jun 2008");
+            
+            const Date<int> b(1, 1, 1600); //0
+            std::ostringstream out2;
+            out2 << b;
+            CPPUNIT_ASSERT(out2.str() == "1 Jan 1600");	                
+
+            const Date<int> y(2, 1, 1601); //367
+            std::ostringstream out3;
+            out3 << y;
+            CPPUNIT_ASSERT(out3.str() == "2 Jan 1601");
+            	                
+            const Date<int> z(2, 1, 3601); //730852
+            std::ostringstream out4;
+            out4 << z;
+            CPPUNIT_ASSERT(out4.str() == "2 Jan 3601");	                            
+
+            const Date<int> a(1, 3, 2000); //146157
+            std::ostringstream out5;
+            out5 << a;
+            CPPUNIT_ASSERT(out5.str() == "1 Mar 2000");
+            	                
+            const Date<int> c(1, 1, 2400); //292194
+            std::ostringstream out6;
+            out6 << c;
+            CPPUNIT_ASSERT(out6.str() == "1 Jan 2400");
+            	                
+            const Date<int> d(1, 1, 2404); //293655
+            std::ostringstream out7;
+            out7 << d;
+            CPPUNIT_ASSERT(out7.str() == "1 Jan 2404");	                
+            
+            const Date<int> e(1, 3, 2600); //365302
+	        std::ostringstream out8;
+            out8 << e;
+            CPPUNIT_ASSERT(out8.str() == "1 Mar 2600");        
+            
+            const Date<int> f(1, 3, 2601); //365667
+            std::ostringstream out9;
+            out9 << f;
+            CPPUNIT_ASSERT(out9.str() == "1 Mar 2601");	                
+
+            const Date<int> ff(1,1,3705);  //768836
+            std::ostringstream out11;
+            out11 << ff;
+            CPPUNIT_ASSERT(out11.str() == "1 Jan 3705");
+
+            const Date<int> xx(7,12,1848);  //90921
+            std::ostringstream out12;
+            out12 << xx;
+            CPPUNIT_ASSERT(out12.str() == "7 Dec 1848");
+            }
         catch (std::invalid_argument& e) {
             CPPUNIT_ASSERT(false);}}
 
@@ -123,6 +255,7 @@ struct TestDate : CppUnit::TestFixture {
 
     CPPUNIT_TEST_SUITE(TestDate);
     CPPUNIT_TEST(test_constructor);
+    CPPUNIT_TEST(test_to_days);
     CPPUNIT_TEST(test_equal_to);
     CPPUNIT_TEST(test_less);
     CPPUNIT_TEST(test_plus);
@@ -146,3 +279,4 @@ int main () {
 
     cout << "Done." << endl;
     return 0;}
+
